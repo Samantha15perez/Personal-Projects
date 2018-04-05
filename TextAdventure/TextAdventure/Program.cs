0c47b62b417530d8ff3195538a6ef65f26ab107e
Samantha15perez <Samantha15perez@gmail.com>
2018-04-04 21:52:03 -0400

    Text-adventure title screen created

---
+++ TextAdventure/TextAdventure/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(@"
+  ██████  ▄████▄   ██▀███   ██▓ ███▄ ▄███▓▓█████▄  ▒█████   ██▀███    █████▒
+▒██    ▒ ▒██▀ ▀█  ▓██ ▒ ██▒▓██▒▓██▒▀█▀ ██▒▒██▀ ██▌▒██▒  ██▒▓██ ▒ ██▒▓██   ▒ 
+░ ▓██▄   ▒▓█    ▄ ▓██ ░▄█ ▒▒██▒▓██    ▓██░░██   █▌▒██░  ██▒▓██ ░▄█ ▒▒████ ░ 
+  ▒   ██▒▒▓▓▄ ▄██▒▒██▀▀█▄  ░██░▒██    ▒██ ░▓█▄   ▌▒██   ██░▒██▀▀█▄  ░▓█▒  ░ 
+▒██████▒▒▒ ▓███▀ ░░██▓ ▒██▒░██░▒██▒   ░██▒░▒████▓ ░ ████▓▒░░██▓ ▒██▒░▒█░    
+▒ ▒▓▒ ▒ ░░ ░▒ ▒  ░░ ▒▓ ░▒▓░░▓  ░ ▒░   ░  ░ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒ ░    
+░ ░▒  ░ ░  ░  ▒     ░▒ ░ ▒░ ▒ ░░  ░      ░ ░ ▒  ▒   ░ ▒ ▒░   ░▒ ░ ▒░ ░      
+░  ░  ░  ░          ░░   ░  ▒ ░░      ░    ░ ░  ░ ░ ░ ░ ▒    ░░   ░  ░ ░    
+      ░  ░ ░         ░      ░         ░      ░        ░ ░     ░             
+         ░                                 ░                                
+
+okay, so the title screen works. lol");
+            Console.Beep(233, 100);
+            Console.Beep(311, 120);
+            Console.Beep(261, 100);
+            Console.Beep(38, 100);
+            Console.Beep(369, 100);
+            Console.Beep(415, 120);
+            Console.Beep(369, 100);
+            Console.Beep(38, 100);
+            Console.Beep(493, 100);
+            Console.Beep(587, 120);
+            Console.Beep(659, 80);
+            Console.Beep(739, 100);
+            Console.Beep(987, 400);
+            Console.Read();
+            
+
+        }
+    }
+}
